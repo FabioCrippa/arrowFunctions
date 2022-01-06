2f7fc2b3022b7cea24b4ed4a34b64cb384c4ec05
FabioCrippa <crippa.fabiocrippa@gmail.com>
2022-01-06 08:57:41 -0300

    primeiro commit

---
+++ exercicios.js
@@ -0,0 +1,42 @@
+//estrutura arrow function
+//let nomeVariavel = (a, b...) => {return a + b} console.log(nomeVariavel(valorA, valorB))
+let soma = (a, b)=> {
+    return a + b;
+}
+console.log(soma(10, 30));
+
+let multiplicar = (a, b) => {
+    return a * b;
+}
+console.log(multiplicar(2, 100));
+
+let dividir = (a, b) => {
+    return a / b;
+}
+console.log(dividir(100, 2));
+
+let subtrair = (a, b) => {
+    return a - b;
+}
+console.log(subtrair(100, 10));
+
+//outro modo de arrow function
+//let nomeVariavel = ()=>console.log();
+//nomeVariavel();
+
+let somar = (a, b)=>console.log(a + b);
+somar(10, 50);
+
+//outro modo de arrow function
+//let nomeVariavel = console.log();
+//nomevariavel()
+//apenas para 1 parametro
+
+let soma = console.log();
+soma();
+
+//function convencional
+function adicao(a, b) {
+     return a + b;
+}
+console.log(adicao(500, 1000));
